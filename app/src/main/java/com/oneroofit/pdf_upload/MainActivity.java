91e19a863a3433b40d3a1701daca6be87b921692
upload pdf in android <amrat014@gmail.com>
2021-01-08 15:02:10 +0530

    final

---
+++ app/src/main/java/com/oneroofit/pdf_upload/MainActivity.java
@@ -0,0 +1,134 @@
+package com.oneroofit.pdf_upload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    Uri path;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button browse = (Button) findViewById(R.id.browse);
+        Button upload = (Button) findViewById(R.id.upload);
+
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent();
+                intent.setType("application/pdf");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select PDF"), 1);
+            }
+        });
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                (new Upload(MainActivity.this, path)).execute();
+
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        super.onActivityResult(requestCode, resultCode, result);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                path = result.getData();
+            }
+        }
+    }
+}
+
+class Upload extends AsyncTask<Void, Void, Void> {
+    private ProgressDialog pd;
+    private Context c;
+    private Uri path;
+
+    public Upload(Context c, Uri path) {
+        this.c = c;
+        this.path = path;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd = ProgressDialog.show(c, "Uploading", "Please Wait");
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        pd.dismiss();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        String url_path = "http://oneroofit.digitalsofthub.online/pdf.php";
+        HttpURLConnection conn = null;
+
+        int maxBufferSize = 1024;
+        try {
+            URL url = new URL(url_path);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setDoInput(true);
+            conn.setChunkedStreamingMode(1024);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "multipart/form-data");
+
+            OutputStream outputStream = conn.getOutputStream();
+            InputStream inputStream = c.getContentResolver().openInputStream(path);
+
+            int bytesAvailable = inputStream.available();
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            byte[] buffer = new byte[bufferSize];
+
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer, 0, bufferSize)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.i("result", line);
+            }
+            reader.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return null;
+    }
+}
